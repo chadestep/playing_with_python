2bf54b471b4542f927eaffe1f3dbaf2e2837be0b
Chad Estep <chadestep@gmail.com>
2015-06-11 12:07:55 -0500

    oopsie

--- step_window.py
+++ step_window.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def step_window(array, window, step):
 	"""
-	This function is built to take a 2D numpy array and convert it
+	This function is built to take a numpy array and convert it
 	to a 3D array where the specified window size will be the new
 	array row length, column number will not change, and the array
 	depth (epochs) will depend on both the window and step size.
@@ -21,12 +21,13 @@ def step_window(array, window, step):
 	"""
 
 	if array.ndim < 2:
+		array = array.reshape(len(array), 1)
 		columns = 1
 	else:
 		columns = array.shape[1]
 	rows = array.shape[0]
 	new_x, new_y = columns, window
-	new_z = int(1 + (rows - window) / step))
+	new_z = int(1 + (rows - window) / step)
 	new_array = np.empty([new_y, new_x, new_z])
 	for x in range(new_z):
 		new_array[:,:,x] = array[(0 + step * x):(window + step * x)]
