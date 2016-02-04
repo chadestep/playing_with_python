8cfa35bdd90d6986e48cbafd57d71cad630d3c64
Chad Estep <chadestep@gmail.com>
2016-02-04 17:55:43 -0600

    totally reworked boxplot function

--- python_scripts/data_plots.py
+++ python_scripts/data_plots.py
@@ -5,10 +5,18 @@ __author__ = "Chad Estep (chadestep@gmail.com)"
 
 import matplotlib.pyplot as plt
 
-def boxplot_all(df):
+def nu_boxplot(df,medians_only=False):
     """
+    Makes a much improved boxplot.
+
     Parameters
     ----------
+    df: pandas DataFrame
+        Pandas Dataframe where each of the columns makes a separate boxplot.
+        Column names will be used as boxplot labels.
+    medians_only: bool (default=False)
+        Default changes the entire boxplot the new color,
+        but if True only changes the color of the median bar.
     """
     data = df.dropna().values
     if df.ndim == 1:
@@ -17,48 +25,35 @@ def boxplot_all(df):
     else:
         column_num = df.shape[1]
         labels = df.columns.values
+# make the basic figure with better default properties.
     f, ax = plt.subplots(1, figsize=(column_num,5))
-    ax.boxplot(data,
+    bp = plt.boxplot(data,
+               boxprops=dict(color='000000',linestyle='-',linewidth=2),
+               flierprops=dict(linestyle='none',marker='.',markeredgecolor='000000',
+                               markerfacecolor='000000',markersize=5),
                labels=labels,
+               medianprops=dict(color='000000',linestyle='-',linewidth=4),
                widths=0.5,
                whis=[10,90],
-               whiskerprops=dict(linestyle='-',linewidth=2,color='black'),
-               boxprops=dict(linestyle='-',linewidth=2,color='black'),
-               medianprops=dict(linestyle='-',linewidth=6,color='red'),
-               flierprops=dict(marker='.',markerfacecolor='black',
-                               markeredgecolor='black',markersize=5,linestyle='none'))
+               whiskerprops=dict(color='000000',linestyle='-',linewidth=2))
+# reset the colors based on user input
+    colors = [i['color'] for i in plt.rcParams['axes.prop_cycle']]
+    if medians_only:
+        for i in range(column_num):
+            color = colors[i]
+            plt.setp(bp['medians'][i], color=color)
+    else:
+        for i in range(column_num):
+            color = colors[i]
+            plt.setp(bp['boxes'][i], color=color)
+            plt.setp(bp['caps'][i*2], color=color)
+            plt.setp(bp['caps'][i*2+1], color=color)
+            plt.setp(bp['fliers'][i], markerfacecolor=color, markeredgecolor=color)
+            plt.setp(bp['medians'][i], color=color)
+            plt.setp(bp['whiskers'][i*2], color=color)
+            plt.setp(bp['whiskers'][i*2+1], color=color)
+
     plt.xticks(rotation=45)
 #     NEED TO MAKE SURE THIS IS CALLED PROPERLY IN THE FINISHED VERSION!
     simple_axis(ax)
     return f, ax
-
-def boxplot_series(df):
-    """
-    """
-    if df.ndim == 1:
-        boxplot_all(df)
-    else:
-        column_num = df.shape[1]
-        data = df.dropna().values
-        labels = df.columns.values
-        colors = [i['color'] for i in plt.rcParams['axes.prop_cycle']]
-        f, ax = plt.subplots(1, column_num, figsize=(column_num,5), sharey=True)
-        for i in range(column_num):
-            color = colors[i]
-            ax[i].boxplot(data[:,i],
-                          widths=0.5,
-                          whis=[10,90],
-                          capprops=dict(color=color),
-                          whiskerprops=dict(linestyle='-',linewidth=2,color=color),
-                          boxprops=dict(linestyle='-',linewidth=2,color=color),
-                          medianprops=dict(linestyle='-',linewidth=6,color=color),
-                          flierprops=dict(marker='.',markerfacecolor=color,markeredgecolor=color,
-                                          markersize=5,linestyle='none'))
-            ax[i].set_xticklabels(labels[i], rotation=45)
-            if i < 1:
-                simple_axis(ax[i])
-            else:
-                simple_axis(ax[i])
-                ax[i].spines['left'].set_visible(False)
-                ax[i].get_yaxis().set_visible(False)
-        return f, ax
