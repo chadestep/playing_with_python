d2f4d29e04e604ee3a8d4e4d17a761ba38267f04
Chad Estep <chadestep@gmail.com>
2016-02-03 22:58:55 -0600

    largely fixed boxplot functions

--- python_scripts/data_plots.py
+++ python_scripts/data_plots.py
@@ -5,67 +5,60 @@ __author__ = "Chad Estep (chadestep@gmail.com)"
 
 import matplotlib.pyplot as plt
 
-def boxplot_all(data_array):
+def boxplot_all(df):
     """
-    Basic functionality is there, but need to make sure it doesn't break,
-    and need to add  optional arguments (like labels, maybe colors)
-
     Parameters
     ----------
-    data_array: 2D list of lists
-
     """
-    whis=[10,90]
-    boxprops = dict(linestyle='-',linewidth=2,color='black')
-    medianprops = dict(linestyle='-',linewidth=6,color='red')
-    flierprops = dict(marker='.',markerfacecolor='black',markeredgecolor='black',markersize=5,linestyle='none')
-    whiskerprops = dict(linestyle='-',linewidth=2,color='black')
-
-    f, ax = plt.subplots(1, figsize=(len(data_array),5))
-    ax.boxplot(data_array,
+    data = df.dropna().values
+    if df.ndim == 1:
+        column_num = 1
+        labels = [df.name]
+    else:
+        column_num = df.shape[1]
+        labels = df.columns.values
+    f, ax = plt.subplots(1, figsize=(column_num,5))
+    ax.boxplot(data,
+               labels=labels,
                widths=0.5,
-               whis=whis,
-               whiskerprops=whiskerprops,
-               boxprops=boxprops,
-               medianprops=medianprops,
-               flierprops=flierprops)
+               whis=[10,90],
+               whiskerprops=dict(linestyle='-',linewidth=2,color='black'),
+               boxprops=dict(linestyle='-',linewidth=2,color='black'),
+               medianprops=dict(linestyle='-',linewidth=6,color='red'),
+               flierprops=dict(marker='.',markerfacecolor='black',
+                               markeredgecolor='black',markersize=5,linestyle='none'))
     plt.xticks(rotation=45)
-#     NEED TO MAKE SURE THIS IS CALLED PROPERLY!
+#     NEED TO MAKE SURE THIS IS CALLED PROPERLY IN THE FINISHED VERSION!
     simple_axis(ax)
     return f, ax
 
-
-def boxplot_series(data_array):
-    """cycles through colors, and makes plots independent but they do share a y-axis
-
+def boxplot_series(df):
     """
-
-    estep_color_list = [i['color'] for i in plt.rcParams['axes.prop_cycle']]
-#     estep_color_list
-
-    whis=[10,90]
-    boxprops = dict(linestyle='-',linewidth=2,color='black')
-    medianprops = dict(linestyle='-',linewidth=6,color=estep_color_list[i])
-    flierprops = dict(marker='.',markerfacecolor='black',markeredgecolor='black',markersize=5,linestyle='none')
-    whiskerprops = dict(linestyle='-',linewidth=2,color='black')
-
-    f, ax = plt.subplots(1,len(data_array), figsize=(len(data_array),5), sharey=True)
-    for i in range(len(data_array)):
-        color = estep_color_list[i]
-        ax[i].boxplot(data_array[i],
-#                       labels=['data'],
-                      widths=0.5,
-                      whis=whis,
-                      capprops=dict(color=color),
-                      whiskerprops=dict(linestyle='-',linewidth=2,color=color),
-                      boxprops=dict(linestyle='-',linewidth=2,color=color),
-                      medianprops=dict(linestyle='-',linewidth=6,color=color),
-                      flierprops=dict(marker='.',markerfacecolor=color,markeredgecolor=color,markersize=5,linestyle='none'))
-        if i < 1:
-            simple_axis(ax[i])
-        else:
-            simple_axis(ax[i])
-    #         a few more to clean up the boarders of the other plots
-            ax[i].spines['left'].set_visible(False)
-            ax[i].get_yaxis().set_visible(False)
-    return f, ax
+    """
+    if df.ndim == 1:
+        boxplot_all(df)
+    else:
+        column_num = df.shape[1]
+        data = df.dropna().values
+        labels = df.columns.values
+        colors = [i['color'] for i in plt.rcParams['axes.prop_cycle']]
+        f, ax = plt.subplots(1, column_num, figsize=(column_num,5), sharey=True)
+        for i in range(column_num):
+            color = colors[i]
+            ax[i].boxplot(data[:,i],
+                          widths=0.5,
+                          whis=[10,90],
+                          capprops=dict(color=color),
+                          whiskerprops=dict(linestyle='-',linewidth=2,color=color),
+                          boxprops=dict(linestyle='-',linewidth=2,color=color),
+                          medianprops=dict(linestyle='-',linewidth=6,color=color),
+                          flierprops=dict(marker='.',markerfacecolor=color,markeredgecolor=color,
+                                          markersize=5,linestyle='none'))
+            ax[i].set_xticklabels(labels[i], rotation=45)
+            if i < 1:
+                simple_axis(ax[i])
+            else:
+                simple_axis(ax[i])
+                ax[i].spines['left'].set_visible(False)
+                ax[i].get_yaxis().set_visible(False)
+        return f, ax
